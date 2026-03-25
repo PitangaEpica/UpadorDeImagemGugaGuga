7f2fed320ed383853af241ed08bd00d6196e7ab9
PitangaEpica <pietromazzochi@gmail.com>
2026-03-25 15:04:36 -0300

    lorenzo123123

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,10 @@
 let input = document.getElementById("fileInput");
 let uploadButton = document.getElementById("uploadButton");
 let image = document.getElementById("image");
+let image2 = document.getElementById("image2");
 let preview = document.getElementById("preview");
+let btnPassword = document.getElementById("btnPassword");
+
 import { addImage } from "./CRUD.js";
 
 uploadButton.addEventListener("click", () => {
@@ -9,12 +12,12 @@ uploadButton.addEventListener("click", () => {
   if (file) {
     let reader = new FileReader();
     reader.onload = (e) => {
-      image.src = e.target.result;
-      addImage(e.target.result);
+      image2.src = e.target.result;
       preview.src = "";
+      document.getElementById("promptSenha").style.display = "flex";
+      document.getElementById("overlay").style.display = "block";
     };
     reader.readAsDataURL(file);
-    input.value = "";
   } else {
     uploadButton.textContent = "Nenhuma imagem selecionada";
     uploadButton.style.color = "rgb(230, 40, 40)";
@@ -24,6 +27,31 @@ uploadButton.addEventListener("click", () => {
     }, 1000);
   }
 });
+
+btnPassword.addEventListener('click', () => {
+  let file = input.files[0];
+  if (file&&document.getElementById("passInput").value == "Marios") {
+    let reader = new FileReader();
+    reader.onload = (e) => {
+      image.src = e.target.result;
+      addImage(e.target.result);
+      document.getElementById("promptSenha").style.display = "none";
+      document.getElementById("overlay").style.display = "none";
+    }
+    reader.readAsDataURL(file);
+    input.value = "";
+  } else if(document.getElementById("passInput").value ==""){
+    alert("Insira uma senha");
+  }
+  else{
+    alert("Senha Incorreta")
+  }
+});
+document.getElementById("overlay").addEventListener('click', ()=>{
+  document.getElementById("promptSenha").style.display = "none";
+  document.getElementById("overlay").style.display = "none";
+})
+
 
 input.addEventListener("change", () => {
   let file = input.files[0];
@@ -36,6 +64,6 @@ input.addEventListener("change", () => {
   }
 });
 
-document.getElementById("changeTab").addEventListener('click',()=>{
-    window.location.href = 'password.html'
-})
+document.getElementById("changeTab").addEventListener("click", () => {
+  window.location.href = "password.html";
+});
